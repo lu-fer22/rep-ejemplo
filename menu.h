7d4ebf52bcaa5a7799a50d0933f3388b382db78b
lu-fer22 <luisamartinezjuarez077@gmail.com>
2026-06-16 00:51:14 -0600

    menu.h

--- menu.h
+++ menu.h
@@ -1,6 +1,20 @@
-#indef menu_p
-#define menu_p
+ifndef MENU_P
+#define MENU_P
 
-int menu();
+#include <stdio.h>
+
+static inline int menu(void) {
+    int op;
+
+    printf("Seleccione una opcion\n");
+    printf("1.- Insertar\n");
+    printf("2.- Mostrar\n");
+    printf("3.- Liberar\n");
+    printf("0.- Salir\n");
+
+    scanf("%d", &op);
+
+    return op;
+}
 
 #endif
